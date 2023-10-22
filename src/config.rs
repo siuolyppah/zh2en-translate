205da10dc86ecde4d8b2dd346417de4e4a6047c4
gxy <2793392818@qq.com>
2023-10-22 19:49:45 +0800

    fix: read home dir from env

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,13 @@
+use once_cell::sync::Lazy;
+
 /// [download here](https://huggingface.co/Helsinki-NLP/opus-mt-zh-en)
-pub const RESOURCE_PATH: &str = "/home/gxy/nlp-resource";
+pub const RESOURCE_PATH: Lazy<String> = Lazy::new(|| match home::home_dir() {
+    Some(mut home) => {
+        home.push("nlp-resource");
+        home.to_str().unwrap().to_owned()
+    }
+    None => unreachable!(),
+});
+
 pub const REMOTE_RESOURCE_URL_PREFIX: &str =
     "https://huggingface.co/Helsinki-NLP/opus-mt-zh-en/raw/main";
